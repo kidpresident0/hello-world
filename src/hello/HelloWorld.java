e8508ca27eec1d5fd32d7df9cf35e669d537d114
John Christian <john.christian1@pcc.edu>
2022-10-17 21:17:13 -0700

    Added author name

--- src/hello/HelloWorld.java
+++ src/hello/HelloWorld.java
@@ -1,6 +1,10 @@
 package hello;
 import java.util.Date;
 
+/**
+ * Author John Christian
+ */
+
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello, World!");
