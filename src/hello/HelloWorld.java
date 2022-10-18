68fdf4fc637bfb5b9f972650e5e73b318c1ff9ee
John Christian <john.christian1@pcc.edu>
2022-10-17 21:07:07 -0700

    print date

--- src/hello/HelloWorld.java
+++ src/hello/HelloWorld.java
@@ -1,7 +1,9 @@
 package hello;
+import java.util.Date;
 
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello, World!");
+        System.out.println(new Date());
     }
 }
